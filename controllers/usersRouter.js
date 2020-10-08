d14d1349e51265696c39a8bec9ec9eeec613cd5c
Hector <hectorpwns@gmail.com>
2020-10-08 16:10:21 -0400

    empty array avoids type error on frontend

--- controllers/usersRouter.js
+++ controllers/usersRouter.js
@@ -66,11 +66,11 @@ usersRouter.get('/lists/:id', async (req, res) => {
   const { id } = req.params
   const userCollection = await pool.query(
     `
-  SELECT movie_lists.title, movie_lists.user_id, json_agg(DISTINCT movies)FROM movie_lists
+  SELECT movie_lists.title, movie_lists.id as list_id, movie_lists.user_id, COALESCE(json_agg(DISTINCT movies.tvdb_movieid)filter(where movies.tvdb_movieid IS NOT NULL),'[]') as movies FROM movie_lists
   LEFT JOIN movie_list_items ON movie_lists.id = movie_list_items.movie_lists_id
   FULL OUTER JOIN movies ON movies.id = movie_list_items.movies_id
   WHERE movie_lists.user_id = $1
-  GROUP BY 1,2 `,
+  GROUP BY 2 `,
     [id]
   )
   res.status(200).json(userCollection.rows)
