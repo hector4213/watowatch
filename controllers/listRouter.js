89887e1b496857f665d808c0803281ad5c2d357d
Hector <hectorpwns@gmail.com>
2020-10-13 21:38:46 -0400

    fixed postgres query to remove movie item fr list

--- controllers/listRouter.js
+++ controllers/listRouter.js
@@ -54,12 +54,13 @@ listRouter.get('/:id', async (req, res) => {
 
 listRouter.put('/:id', async (req, res) => {
   const { id } = req.params
-  const { title, genre, tvdbid } = req.body
+  console.log(id)
+  const { title, genre, tvdb_movieid } = req.body
   const decodedToken = jwt.verify(req.token, process.env.SECRET)
   if (!req.token) {
     return res.status(401).json({ error: 'token missing or invalid' })
   }
-  if (!title || !genre || !tvdbid || !decodedToken) {
+  if (!title || !genre || !tvdb_movieid || !decodedToken) {
     return res.status(400).json({ error: 'missing content' })
   }
 
@@ -81,34 +82,36 @@ listRouter.put('/:id', async (req, res) => {
   )
 
   if (isListAuthor.rows.length > 0 || isBuddy.rows.length > 0) {
-    const isDuplicate = await pool.query(
+    const addMovie = await pool.query(
       `
-       SELECT movies.title
-       FROM movie_lists
-       INNER JOIN movie_list_items ON movie_lists.id = movie_list_items.movie_lists_id
-       INNER JOIN movies ON movies.id = movie_list_items.movies_id
-       WHERE movies.title = $1
-    `,
-      [title]
-    )
-    if (isDuplicate.rows.length < 1) {
-      const addMovie = await pool.query(
-        `
         INSERT INTO movies(title, genre, tvdb_movieid) VALUES($1, $2, $3) RETURNING id 
         `,
-        [title, genre, tvdbid]
-      )
+      [title, genre, tvdb_movieid]
+    )
+
+    const isInList = await pool.query(
+      `
+      SELECT movies.tvdb_movieid FROM movie_lists
+      INNER JOIN movie_list_items ON movie_lists.id = movie_list_items.movie_lists_id
+      INNER JOIN movies ON movie_list_items.movies_id = movies.id
+      WHERE movie_lists.user_id = $1
+      GROUP BY movie_lists.user_id, movie_lists.id, movies.id
+        `,
+      [decodedToken.id]
+    )
+    const inList = isInList.rows.includes(tvdb_movieid)
+
+    if (!inList) {
       const addToListItems = await pool.query(
         `
-        INSERT INTO movie_list_items(movie_lists_id, movies_id) VALUES($1, $2)
-        `,
+          INSERT INTO movie_list_items(movie_lists_id, movies_id) VALUES($1, $2)
+          `,
         [id, addMovie.rows[0].id]
       )
       return res.status(204).json(addMovie.rows[0].id)
     }
-    res.status(400).json({ error: 'Item already in list' })
   }
-  res.status(401).json({ error: 'User is neither author or a buddy' })
+  return res.status(401).json({ error: 'User is neither author or a buddy' })
 })
 
 //Add a movie buddie to list OK
@@ -221,8 +224,8 @@ listRouter.delete('/:id', async (req, res) => {
   res.status(401).json({ error: 'List can only be deleted by author' })
 })
 
-//Remove a movie by either author or buddy
-listRouter.delete('/:id/movies', async (req, res) => {
+//Remove a movie by either author or buddy(editing is why put request )
+listRouter.put('/:id/movies', async (req, res) => {
   const { id } = req.params
   const { movieId } = req.body
   const decodedToken = jwt.verify(req.token, process.env.SECRET)
@@ -252,13 +255,19 @@ listRouter.delete('/:id/movies', async (req, res) => {
     const deletedListItem = await pool.query(
       `
       DELETE FROM movie_list_items
-      WHERE movie_list_items.movie_lists_id = $1 and movie_list_items.movies_id = $2 RETURNING *
+      WHERE movie_list_items.movies_id IN (
+      SELECT movie_list_items.movies_id FROM movie_list_items
+      INNER JOIN movies ON movies.id = movie_list_items.movies_id
+      WHERE movie_list_items.movie_lists_id = $1 AND movies.tvdb_movieid = $2
+)
       `,
       [id, movieId]
     )
     return res.status(204)
   }
-  res.status(401).json({ error: 'Only an author or buddy can delete a movie' })
+  return res
+    .status(401)
+    .json({ error: 'Only an author or buddy can delete a movie' })
 })
 
 module.exports = listRouter
